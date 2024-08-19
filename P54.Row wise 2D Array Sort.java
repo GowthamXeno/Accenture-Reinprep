7ad4abcb59a18767425412e953c85b1909145f1f
Gowtham M <101506428+GowthamXeno@users.noreply.github.com>
2024-08-19 17:14:05 +0530

    Create P54.Row wise 2D Array Sort.java

---
+++ P54.Row wise 2D Array Sort.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        int n = scan.nextInt();
+        int[][] arr = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = scan.nextInt();
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            Arrays.sort(arr[i]);
+        }
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+//testcase passed : 1/5 (May be a bug)
